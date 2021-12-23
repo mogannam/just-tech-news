4bbdf833f85c3a670edf1306b7a83ee0cc2b71df
mogannam <JoeMogannam@gmail.com>
2021-12-22 17:24:11 -0800

    Login feature with MVC

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));     
 app.use(express.urlencoded({ extended: true }));
 
 // turn on routes
 app.use(routes);
+
+// /********* handelbars */
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// /********  */
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
